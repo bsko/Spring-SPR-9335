33db0ac21903b8929c436e3219f541d311e03b52
bsko <adobeangryslave@gmail.com>
2013-08-27 18:37:32 +0400

    Removed autowired service field in Route class

--- project/bank-products/src/main/java/ru/bsc/mbs/jmba/service/bank/products/BankProductsRoute.java
+++ project/bank-products/src/main/java/ru/bsc/mbs/jmba/service/bank/products/BankProductsRoute.java
@@ -1,14 +1,10 @@
 package ru.bsc.mbs.jmba.service.bank.products;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import ru.bsc.mbs.jmba.protocol.route.AbstractRoute;
 
 /**
  * @author R. Malyshev
  */
 public class BankProductsRoute extends AbstractRoute {
-
-  @Autowired
-  private BankProductsService bankProductsService;
 
 }
